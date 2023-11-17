22db7928f1bd92e15bff938967d1325201ea8349
SergeyPesotskiy <mainerall@gmail.com>
2023-11-17 21:28:16 +0600

    copyPassport

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let copyPassport = { ...passport };
+
+copyPassport.name = "Ivan";
+
+console.log(passport);
+
+console.log(copyPassport);
