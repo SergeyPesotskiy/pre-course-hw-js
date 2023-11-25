65105ca36ad401031a9dd1aa43af69fe94369344
SergeyPesotskiy <mainerall@gmail.com>
2023-11-25 20:54:27 +0600

    germaFlag

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function creatColorString() {
+    let germaFlag = colors.filter ( function(color) { return color === "черный" || color === "красный" || color === "желтый";    });
+    return germaFlag.join('-');
+}
+
+console.log(creatColorString());
