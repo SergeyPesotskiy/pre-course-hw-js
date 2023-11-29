bcaf79879d856842fd21a3e6e20462752d39e204
SergeyPesotskiy <mainerall@gmail.com>
2023-11-29 11:59:48 +0600

    numbers[x][y] % 2 !== 0

---
+++ lesson-03/07.js
@@ -0,0 +1,17 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+let numbersAnswer = [];
+
+for (let x = 0; x < numbers.length; x++ ) {
+    for (let y = 0; y < numbers[x].length; y++) {
+        if (numbers[x][y] % 2 !== 0) {
+            numbersAnswer.push(numbers[x][y]);
+        }
+    }
+}
+
+console.log(numbersAnswer);
